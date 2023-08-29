2442ccc2873865eca483cdc2ca36da5514ce4466
Rennan Ventura <rennan.ventura.alves@gmail.com>
2023-08-29 16:33:16 -0300

    Refatorando o codigo criando funções

--- fogefoge.c
+++ fogefoge.c
@@ -1,13 +1,30 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include"fogefoge.h"
 
 
 char** mapa;
 int linhas;
 int colunas; 
 
-int main(){
 
+void liberaMapa(){
+    for (int i = 0; i < linhas; i++)
+    {
+        free(mapa[i]);
+    }
+    free(mapa);
+}
+
+void alocaMapa(){
+    mapa = malloc(sizeof(char*) * linhas);
+    for (int i = 0; i < linhas; i++)
+    {
+        mapa[i] = malloc(sizeof(char) * (colunas + 1));
+    }
+}
+
+void leMapa(){
     FILE* f = fopen("mapa.txt", "r");
     if(f == 0) {
         printf("Erro na leitura do mapa\n");
@@ -17,24 +34,22 @@ int main(){
     fscanf(f, "%d %d", &linhas, &colunas);
     printf("linhas %d colunas %d\n", linhas, colunas);
 
-    mapa = malloc(sizeof(char*) * linhas);
-    for (int i = 0; i < linhas; i++)
-    {
-        mapa[i] = malloc(sizeof(char) * (colunas + 1));
-    }
+    alocaMapa();
   
     for(int i = 0; i < 5; i++){
         fscanf(f, "%s", mapa[i]);
     }
+    fclose(f);
+}
+
+int main(){
+
+    leMapa();
+
     for(int i = 0; i < 5; i++){
         printf("%s\n", mapa[i]);
     }
-    fclose(f);
 
-    for (int i = 0; i < linhas; i++)
-    {
-        free(mapa[i]);
-    }
-    free(mapa);
+    liberaMapa();
     
 }
